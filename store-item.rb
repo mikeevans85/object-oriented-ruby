9ce7cc1d0303dbd1b00fdb84126e52cafd379ce5
mikeevans85 <baudecordier@gmail.com>
2018-10-03 20:11:45 -0500

    Added puts

--- store-item.rb
+++ store-item.rb
@@ -1,4 +1,10 @@
 quilt_1 = {:color => "red", :size => "medium", :price => 500, :fabric => "cotton"}
 quilt_2 = {:color => "green", :size => "large", :price => 900, :fabric => "polyester"}
-quilt_3 = {color: => "mauve", size: => "small", price: => 250, fabric: => "acrylic"}
+quilt_3 = {color: "mauve", size: "small", price: 250, fabric: "acrylic"}
+
+puts "A #{quilt_1[:size]} #{quilt_1[:color]} #{quilt_1[:fabric]} quilt is being sold for #{quilt_1[:price]} dollars."
+puts "A #{quilt_2[:size]} #{quilt_2[:color]} #{quilt_2[:fabric]} quilt is being sold for #{quilt_2[:price]} dollars."
+puts "A #{quilt_3[:size]} #{quilt_3[:color]} #{quilt_3[:fabric]} quilt is being sold for #{quilt_3[:price]} dollars."
+
+
 
