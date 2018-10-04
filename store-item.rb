8862bc2d9bdd0b4c77cb0dbf3096cc19fdadd422
mikeevans85 <baudecordier@gmail.com>
2018-10-03 20:06:07 -0500

    Added list

---
+++ store-item.rb
@@ -0,0 +1,4 @@
+quilt_1 = {:color => "red", :size => "medium", :price => 500, :fabric => "cotton"}
+quilt_2 = {:color => "green", :size => "large", :price => 900, :fabric => "polyester"}
+quilt_3 = {color: => "mauve", size: => "small", price: => 250, fabric: => "acrylic"}
+
