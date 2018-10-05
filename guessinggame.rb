347c5a17ffb91417e9ff1bc6e1cab91fabb2d1d6
mikeevans85 <baudecordier@gmail.com>
2018-10-04 19:18:04 -0500

    Add new file

--- guessinggame.rb
+++ guessinggame.rb
@@ -1,13 +1,33 @@
 class GuessingGame
 
   def initialize  
-    answer = rand * 100
-    p answer
+    @answer = rand(100).to_i
+  end
+
+  def answer
+    return @answer
   end
 
 
-  def guess=(input) 
+  def guess(input) 
+    if input < answer
+      puts "Too Low!"
+    elsif input > answer
+      puts "Too high!"
+    else
+      puts "Correct!"
+    end
+  end
 
+  def reset_answer
+    @answer = rand(100).to_i
   end
 
 end
+
+game = GuessingGame.new
+
+game.guess(35)
+game
+
+
--- store-item.rb
+++ store-item.rb
@@ -1,6 +1,6 @@
-quilt_1 = {:color => "red", :size => "medium", :price => 500, :fabric => "cotton"}
-quilt_2 = {:color => "green", :size => "large", :price => 900, :fabric => "polyester"}
-quilt_3 = {color: "mauve", size: "small", price: 250, fabric: "acrylic"}
+# quilt_1 = {:color => "red", :size => "medium", :price => 500, :fabric => "cotton"}
+# quilt_2 = {:color => "green", :size => "large", :price => 900, :fabric => "polyester"}
+# quilt_3 = {color: "mauve", size: "small", price: 250, fabric: "acrylic"}
 
 # puts "A #{quilt_1[:size]} #{quilt_1[:color]} #{quilt_1[:fabric]} quilt is being sold for #{quilt_1[:price]} dollars."
 # puts "A #{quilt_2[:size]} #{quilt_2[:color]} #{quilt_2[:fabric]} quilt is being sold for #{quilt_2[:price]} dollars."
@@ -19,9 +19,9 @@ class Quilt
 
 end
 
-quilt_1 = Quilt.new("red", "medium", 500, "cotton")
-quilt_2 = Quilt.new("green", "large", 900, "polyester")
-quilt_3 = Quilt.new("mauve", "small", 250, "acrylic")
+quilt1 = Quilt.new("red", "medium", 500, "cotton")
+quilt2 = Quilt.new("green", "large", 900, "polyester")
+quilt3 = Quilt.new("mauve", "small", 250, "acrylic")
 
 puts quilt_1.size
 puts quilt_3.color
